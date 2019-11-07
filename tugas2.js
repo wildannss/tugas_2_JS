a663521ead436911d90b3552861eb0318d717df5
wildan <wildan.syarif1021@gmail.com>
2019-11-07 14:52:21 +0700

    tugas niomic
    tugas niomic

---
+++ tugas2.js
@@ -0,0 +1,13 @@
+let tipeNumber = 99;
+let tipeString = 'Ini Tipe String';
+let tipeBoolean = true;
+let tipeNull = null;
+let tipeUndifined;
+let tipeArray = ['ini adalah','tipe array'];
+
+console.log(tipeNumber);
+console.log(tipeString);
+console.log(tipeBoolean);
+console.log(tipeNull);
+console.log(tipeUndifined);
+console.log(tipeArray);
